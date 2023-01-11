68b5c9579818e692aa266630adb86518c137b7b8
Fromgge <gapjames1@gmail.com>
2023-01-11 14:04:07 +0200

    Homework

--- App/Controllers/ParksController.php
+++ App/Controllers/ParksController.php
@@ -4,12 +4,13 @@ namespace App\Controllers;
 
 use App\Models\Car;
 use Core\Controller;
+use Core\Model;
 
 class ParksController extends Controller
 {
     public function show(int $id)
     {
-        dd(Car::select(['model', 'price'])->where('price', '>', 15)->get());
+//        dd(Car::select(['model', 'price'])->where('price', '>', 15)->get());
+        d(Car::all());
     }
-
 }
